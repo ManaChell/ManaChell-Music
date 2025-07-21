783bf2f255c855eb181c1f8c1317b15a0bbc0815
ManaChell <125026386+ManaChell@users.noreply.github.com>
2025-07-21 16:38:34 +0800

    v0.4
    正在建库

---
+++ ManaChell Music Console/ManaChell Music Console.cpp
@@ -0,0 +1,60 @@
+﻿#include<cmio.h>
+struct str:string
+{
+	using string::string;
+	vector<str> block;
+	operator const char* () const
+	{
+		return c_str();
+	}
+	int cunt(char input)
+	{
+		return count(begin(), end(), input);
+	}
+	vector<str> split(const char* input)
+	{
+		vector<str> output;
+		char* copys = new char[this->length() + 1];
+		strcpy(copys, this->c_str());
+		char* cache = strtok(copys, input);
+		str cacheA;
+		while (cache != NULL)
+		{
+			cacheA = cache;
+			output.push_back(cacheA);
+			cache = strtok(NULL, input);
+		}
+		delete[]copys;
+		return output;
+	}
+	void splitA(const char* input)
+	{
+		vector<str>().swap(block);
+		char* copys = new char[this->length() + 1];
+		strcpy(copys, this->c_str());
+		char* cache = strtok(copys, input);
+		str cacheA;
+		while (cache != NULL)
+		{
+			cacheA = cache;
+			block.push_back(cacheA);
+			cache = strtok(NULL, input);
+		}
+		delete[]copys;
+	}
+	void remove(const char* input,int i)
+	{
+		splitA(input);
+		block.erase(block.begin() + i);
+		str cache;
+		for (int k = 0; k < block.size(); k++)
+		{
+			cache += block[k] + input;
+		}
+		*this = cache;
+	}
+};
+int main()
+{
+
+}
